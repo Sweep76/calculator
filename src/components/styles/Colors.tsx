2dbab99c37c1c7fb268c4f19e51abede18d09ed5
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-06 23:28:10 +0800

    chore: added color scheme

---
+++ src/components/styles/Colors.tsx
@@ -0,0 +1,11 @@
+export const myColors = {
+    light: '#F1f2f3',
+    dark: '#2c3e50',
+    blue: '#3498db',
+    btnGray: '#bdc3c7',
+    btnDark: '#34495e',
+    gray: '#7f8c8d',
+    black: '#000',
+    white: '#fff',
+    result: '#2ecc71',
+}
