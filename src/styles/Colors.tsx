b151f8d287fe1cd94cf7f9db02a156c22840ba7b
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-07 01:27:29 +0800

    updated color pallete

--- src/styles/Colors.tsx
+++ src/styles/Colors.tsx
@@ -1,8 +1,8 @@
 export const myColors = {
-    light: '#F1F2F3',
+    light: '#F1F2F3', // Softer white
     dark: '#17171C',
     blue: '#4B5EFC',
-    btnGray: '#4E5EFC',
+    btnGray: '#4E505F',
     btnDark: '#2E2F38',
     gray: '#747477',
     black: '#000000',
